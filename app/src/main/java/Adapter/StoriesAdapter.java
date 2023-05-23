51d2954f5b165968f9310013510ba7ca2ebacf54
tsimikas <hayderus722@gmail.com>
2023-05-24 01:45:35 +0200

    Added all the stories

--- app/src/main/java/Adapter/StoriesAdapter.java
+++ app/src/main/java/Adapter/StoriesAdapter.java
@@ -45,6 +45,9 @@ public class StoriesAdapter extends RecyclerView.Adapter<StoriesAdapter.StoriesV
 
     }
 
+
+
+
     @Override
     public StoriesAdapter.StoriesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.item_stories,parent,false);
--- app/src/main/java/com/hayder/kidsstories/Data.java
+++ app/src/main/java/com/hayder/kidsstories/Data.java
@@ -19,34 +19,63 @@ public class Data {
     }
 
     static {
-        storyList.add(new Story("Blanch Neige", "Une histoire sur une princesse dune très grande beauté", R.drawable.blancheneige, new Scene[]
+        storyList.add(new Story("Blanch Neige", "A story about a princess of great beauty", R.drawable.blancheneige, new Scene[]
                 {
-                        new Scene("Scene 1", "Rachid is the handsome guy i've ever met", R.drawable.blancheneige),
-                        new Scene("Scene 2", "description2", R.drawable.blancheneige),
-                        new Scene("Scene 3", "description3", R.drawable.blancheneige),
+                        new Scene("Scene 1", "Once upon a time there was an evil queen who questioned her magic mirror every day:\n" +
+                                "“Mirror, who is the most beautiful in my kingdom? »\n" +
+                                "  One day, the mirror answers him:\n" +
+                                "\"Snow White is the prettiest.\"", R.drawable.bn1),
+                        new Scene("Scene 2", "The queen flew into a violent rage. She orders her gamekeeper to kill Snow White. At the last moment, he doesn't have the courage and lets Snow White run away. Terrified, the\n" +
+                                "young girl is taken in by forest animals. They lead her to the house of the seven\n" +
+                                "dwarfs. These work in a diamond mine. Snow White settles in and puts\n" +
+                                "order in the house.", R.drawable.bn2),
+                        new Scene("Scene 3", "The queen learns from her mirror that Snow White is still alive. She uses\n" +
+                                "spells to change into an old woman. One day she brings him an apple\n" +
+                                "poisonous which plunges Snow White into a deep sleep.\n" +
+                                "  The dwarves arrive too late. They pursue the witch who falls over a precipice. Then\n" +
+                                "they lock up Snow White in a glass coffin.", R.drawable.bn3),
+                        new Scene("Scene 4", "Mais lorsque le prince charmant embrasse Blanche-Neige, elle se réveille.", R.drawable.bn4),
                 }));
-        storyList.add(new Story("Chaperon Rouge", "Une jeune fille qui rencontre un loup en chemin", R.drawable.chaperonrouje, new Scene[]
-                {new Scene("Scene 1", "description1", R.drawable.chaperonrouje),
-                        new Scene("Scene 2", "description2", R.drawable.chaperonrouje),
-                        new Scene("Scene 3", "description3", R.drawable.chaperonrouje),
+        storyList.add(new Story("Red Riding Hood", "A young girl who meets a wolf on the way", R.drawable.chaperonrouje, new Scene[]
+                {new Scene("Scene 1", "The prettiest little girl in the village, Little Red Riding Hood,\n" +
+                        "leaves to visit her sick grandmother at her mother's request.\n" +
+                        "Her mission is to bring him a pancake and a small pot of butter and to do this,\n" +
+                        "she must cross the village through the forest.", R.drawable.cr1),
+                        new Scene("Scene 2", "Little Red Riding Hood will cross paths with the Wolf and will not be suspicious.\n" +
+                                "She gives him directions on his destination and accepts the Wolf's proposal, which is akin to a game.", R.drawable.cr2),
+                        new Scene("Scene 3", "When she arrives at her destination, the Wolf has already devoured the grandmother and taken her place in her bed.\n" +
+                                "When the little girl finally arrives, she falls into the trap and is devoured in turn.", R.drawable.cr3),
                 }));
-        storyList.add(new Story("Le Chat Botté", "Un chat astucieux qui aide son maître à devenir riche", R.drawable.chat, new Scene[]
+        storyList.add(new Story("Leon is not well", "The story of Leon.", R.drawable.leon6, new Scene[]
                 {
-                        new Scene("Scene 1", "description1", R.drawable.chat),
-                        new Scene("Scene 2", "description2", R.drawable.chat),
-                        new Scene("Scene 3", "description3", R.drawable.chat),
+                        new Scene("Scene 1","Léon is my best friend, he is in the same class as me. We laugh all the time, we play and sometimes we even go home together after school because we live next door. We've known each other since we were little, even if we're not very big.", R.drawable.leon1),
+                        new Scene("Scene 2","For the past few weeks, Léon has been a little weird.." , R.drawable.leon2),
+                        new Scene("Scene 3","He doesn't laugh with me anymore. He looks so sad and when I talk to him, he refuses to answer..." , R.drawable.leon3),
+                        new Scene("Scene 4", "I know playing ball is his favorite game. So I had to propose to her! Yet he remains alone at recess and no longer wants to play with me.", R.drawable.leon4),
+                        new Scene("Scene 5","In the canteen, Léon doesn't eat much anymore. I give him my dessert thinking that it would make him happy to have a second chocolate cookie. I know he loves them, but he doesn't want them." , R.drawable.leon5),
+                        new Scene("Scene 6","As we were coming home from recess, I went to the toilet and it was very, very rushed! I find myself face to face with Leon, who comes out of the toilet with wet eyes. I decide to talk to the teacher about it." , R.drawable.leon6),
+                        new Scene("Scene 7","If you see that one of your friends no longer wants to play, loses their appetite or cries often. Tell an adult, your mistress, one of your parents or 119 (National Telephone Reception Service for Children in Danger) because they can help." , R.drawable.leon7),
                 }));
-        storyList.add(new Story("Le Vilain Petit Canard", "Canard qui se sent différent des autres canards", R.drawable.petit_canard, new Scene[]
+
+        storyList.add(new Story("The Wolf and the Lamb", ".", R.drawable.louplogo, new Scene[]
                 {
-                        new Scene("Scene 1", "description1", R.drawable.petit_canard),
-                        new Scene("Scene 2", "description2", R.drawable.petit_canard),
-                        new Scene("Scene 3", "description3", R.drawable.petit_canard),
-                }));
-        storyList.add(new Story("La Belle et la Bête", "Histoire d'amour entre une femme et une bete", R.drawable.belle_bete, new Scene[]
-                {
-                        new Scene("Scene 1", "description1", R.drawable.belle_bete),
-                        new Scene("Scene 2", "description2", R.drawable.belle_bete),
-                        new Scene("Scene 3", "description3", R.drawable.belle_bete),
+                        new Scene("Scene 1", "The reason of the strongest is always the best :\n" +
+                                "We will show it presently.", R.drawable.loup1),
+                        new Scene("Scene 2", "A Lamb quenched his thirst in the current of a pure wave. A wolf comes fasting looking for adventure.", R.drawable.loup2),
+                        new Scene("Scene 3", "And that hunger in these places attracted.\n" +
+                                "Who makes you so bold to disturb my drink?\n" +
+                                "Says this animal full of rage:\n" +
+                                "You will be punished for your temerity.", R.drawable.loup3),
+                        new Scene("Scene 4", "— Sire, replies the Lamb, may your Majesty\n" +
+                                "Don't get angry;\n" +
+                                "But rather that she considers that I am quenching my thirst in the current, More than twenty paces below Her, And that consequently, in no way, I cannot disturb her drink.", R.drawable.loup4),
+                        new Scene("Scene 5", "\"You disturb her,\" resumed this cruel beast, \"And I know that you slander me last year.\"\n" +
+                                "\"How would I have done it if I hadn't been born?\" Said the Lamb, I'm still suckling my mother.\n" +
+                                "\"If it's not you, then it's your brother.\"\n" +
+                                "— I have none.\n" +
+                                "\"So it's one of yours:", R.drawable.loup5),
+                        new Scene("Scene 6", "For you hardly spare me, You, your shepherds, and your dogs. I have been told: I must take my revenge.\n" +
+                                "Thereupon, in the depths of the forest The Wolf takes it away, and then eats it, Without any other form of trial.", R.drawable.loup6),
 
                 }));
 
